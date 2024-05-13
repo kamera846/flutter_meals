8f046c77137265e98cb15f55ef9b8ae5c7c4de17
M Rafli Ramadani <keerouk.ink@gmail.com>
2024-05-13 14:02:01 +0700

    Setup skeleton & add dummy data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -16,12 +17,12 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
         children: const [
-          Text("1", style: TextStyle(color: Colors.white)),
-          Text("2", style: TextStyle(color: Colors.white)),
-          Text("3", style: TextStyle(color: Colors.white)),
-          Text("4", style: TextStyle(color: Colors.white)),
-          Text("5", style: TextStyle(color: Colors.white)),
-          Text("6", style: TextStyle(color: Colors.white)),
+          CategoryGridItem("1"),
+          CategoryGridItem("2"),
+          CategoryGridItem("3"),
+          CategoryGridItem("4"),
+          CategoryGridItem("5"),
+          CategoryGridItem("6"),
         ],
       ),
     );
