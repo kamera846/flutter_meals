c3f1fa047642edeb8fdb27349b8825c0e704efb4
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2024-07-30 17:10:04 +0700

    Creating a More Complex Provider with StateNotifier

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_meals/models/meal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealsFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favorieMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
